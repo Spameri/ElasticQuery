4c5dc358209a81bbf754cfe9afa37137732261df
VBoss <vcevela@gmail.com>
2018-07-22 23:22:18 +0200

    :boom: WIP

---
+++ src/Document.php
@@ -0,0 +1,74 @@
+<?php declare(strict_types = 1);
+
+namespace Spameri\ElasticQuery;
+
+
+class Document implements \Spameri\ElasticQuery\Entity\ArrayInterface
+{
+
+	/**
+	 * @var ?string
+	 */
+	private $index;
+	/**
+	 * @var ?\Spameri\ElasticQuery\Document\BodyInterface
+	 */
+	private $body;
+	/**
+	 * @var ?string
+	 */
+	private $type;
+	/**
+	 * @var ?string
+	 */
+	private $id;
+	/**
+	 * @var array
+	 */
+	private $options;
+
+
+	public function __construct(
+		?string $index
+		, ?\Spameri\ElasticQuery\Document\BodyInterface $body = NULL
+		, ?string $type = NULL
+		, ?string $id = NULL
+		, array $options = []
+	)
+	{
+		$this->index = $index;
+		$this->body = $body;
+		$this->type = $type;
+		$this->id = $id;
+		$this->options = $options;
+	}
+
+
+	public function toArray() : array
+	{
+		$array = [];
+
+		if ($this->index) {
+			$array['index'] = $this->index;
+		}
+
+		if ($this->body) {
+			$array['body'] = $this->body->toArray();
+		}
+
+		if ($this->type) {
+			$array['type'] = $this->type;
+		}
+
+		if ($this->id) {
+			$array['id'] = $this->id;
+		}
+
+		if ($this->options) {
+			$array = \array_merge($array, $this->options);
+		}
+
+		return $array;
+	}
+
+}
---
+++ src/Document/Body/Plain.php
@@ -0,0 +1,28 @@
+<?php declare(strict_types = 1);
+
+namespace Spameri\ElasticQuery\Document\Body;
+
+
+class Plain implements \Spameri\ElasticQuery\Document\BodyInterface
+{
+
+	/**
+	 * @var array
+	 */
+	private $parameters;
+
+
+	public function __construct(
+		array $parameters
+	)
+	{
+		$this->parameters = $parameters;
+	}
+
+
+	public function toArray() : array
+	{
+		return $this->parameters;
+	}
+
+}
