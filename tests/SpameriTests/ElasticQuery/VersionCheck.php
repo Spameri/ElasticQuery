436ed245e38f6d2bdef80dc8cdd217e0ee0b5117
VBoss <vcevela@gmail.com>
2019-11-05 01:51:02 +0100

    Added tool for checking version in test environments

---
+++ tests/SpameriTests/ElasticQuery/VersionCheck.php
@@ -0,0 +1,31 @@
+<?php declare(strict_types = 1);
+
+namespace SpameriTests\ElasticQuery;
+
+class VersionCheck
+{
+
+	/**
+	 * @var \Spameri\ElasticQuery\Response\ResultMapper
+	 */
+	private static $resultMapper;
+
+
+	public static function check() : \Spameri\ElasticQuery\Response\ResultVersion
+	{
+		if (self::$resultMapper === NULL) {
+			self::$resultMapper = new \Spameri\ElasticQuery\Response\ResultMapper();
+		}
+
+		/** @var \Spameri\ElasticQuery\Response\ResultVersion $resultObject */
+		$resultObject = self::$resultMapper->map(
+			\json_decode(
+				\file_get_contents('http://127.0.0.1:9200'),
+				TRUE
+			)
+		);
+
+		return $resultObject;
+	}
+
+}
