83e26eb09817042a15a02b60729e8e96914828b7
VBoss <vcevela@gmail.com>
2019-11-30 16:16:28 +0100

    EdgeNgram configurable grams

--- src/Mapping/Filter/EdgeNgram.php
+++ src/Mapping/Filter/EdgeNgram.php
@@ -8,9 +8,48 @@ namespace Spameri\ElasticQuery\Mapping\Filter;
 class EdgeNgram implements \Spameri\ElasticQuery\Mapping\FilterInterface
 {
 
+	/**
+	 * @var int
+	 */
+	private $minGram;
+
+	/**
+	 * @var int
+	 */
+	private $maxGram;
+
+
+	public function __construct(
+		int $minGram = 2,
+		int $maxGram = 6
+	)
+	{
+		$this->minGram = $minGram;
+		$this->maxGram = $maxGram;
+	}
+
+
 	public function getType(): string
 	{
 		return 'edge_ngram';
+	}
+
+
+	public function key(): string
+	{
+		return 'customEdgeNgram';
+	}
+
+
+	public function toArray(): array
+	{
+		return [
+			$this->key() => [
+				'type' => 'edge_ngram',
+				'min_gram' => $this->minGram,
+				'max_gram' => $this->maxGram,
+			],
+		];
 	}
 
 }
