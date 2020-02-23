6505cbdeed10b1dd7d48b2b9271c955602f6e438
VBoss <vcevela@gmail.com>
2020-02-23 23:52:35 +0100

    Exists query

---
+++ src/Query/Exists.php
@@ -0,0 +1,41 @@
+<?php declare(strict_types = 1);
+
+namespace Spameri\ElasticQuery\Query;
+
+
+/**
+ * @see https://www.elastic.co/guide/en/elasticsearch/reference/current/query-dsl-exists-query.html
+ */
+class Exists implements LeafQueryInterface
+{
+
+	/**
+	 * @var string
+	 */
+	private $field;
+
+
+	public function __construct(
+		string $field
+	)
+	{
+		$this->field = $field;
+	}
+
+
+	public function key() : string
+	{
+		return 'exits_' . $this->field;
+	}
+
+
+	public function toArray() : array
+	{
+		return [
+			'exists' => [
+				'field' => $this->field,
+			],
+		];
+	}
+
+}
