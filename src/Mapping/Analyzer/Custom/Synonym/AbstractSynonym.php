a51b4d457685f88ac210b44bd1056e4dbcabcecc
VBoss <vcevela@gmail.com>
2020-02-29 08:46:18 +0100

    SynonymAnalyzer and czech implementation

---
+++ src/Mapping/Analyzer/Custom/Synonym/AbstractSynonym.php
@@ -0,0 +1,80 @@
+<?php declare(strict_types = 1);
+
+namespace Spameri\ElasticQuery\Mapping\Analyzer\Custom\Synonym;
+
+abstract class AbstractSynonym
+	implements \Spameri\ElasticQuery\Mapping\CustomAnalyzerInterface,
+	\Spameri\ElasticQuery\Collection\Item
+{
+
+	/**
+	 * @var \Spameri\ElasticQuery\Mapping\Settings\Analysis\FilterCollection
+	 */
+	protected $filter;
+
+	/**
+	 * @var \Spameri\ElasticQuery\Mapping\Filter\Stop
+	 */
+	protected $stopFilter;
+
+	/**
+	 * @var array
+	 */
+	protected $synonyms;
+
+
+	public function __construct(
+		?\Spameri\ElasticQuery\Mapping\Filter\Stop $stopFilter = NULL,
+		array $synonyms
+	)
+	{
+		$this->stopFilter = $stopFilter;
+		$this->synonyms = $synonyms;
+	}
+
+
+	public function key() : string
+	{
+		return $this->name();
+	}
+
+
+	public function getType() : string
+	{
+		return 'custom';
+	}
+
+
+	public function tokenizer() : string
+	{
+		return 'standard';
+	}
+
+
+	public function toArray() : array
+	{
+		$filterArray = [];
+		/** @var \Spameri\ElasticQuery\Mapping\FilterInterface $filter */
+		foreach ($this->filter() as $filter) {
+			if ($filter instanceof \Spameri\ElasticQuery\Mapping\Filter\Synonym) {
+				$filterArray[] = $filter->getName();
+
+			} elseif ($filter instanceof \Spameri\ElasticQuery\Mapping\Filter\Stop) {
+				$filterArray[] = $filter->getName();
+
+			} else {
+				$filterArray[] = $filter->getType();
+			}
+		}
+
+		return [
+			$this->name() => [
+				'type'      => $this->getType(),
+				'tokenizer' => $this->tokenizer(),
+				'filter'    => $filterArray,
+			],
+		];
+	}
+
+
+}
---
+++ src/Mapping/Analyzer/Custom/Synonym/CzechSynonym.php
@@ -0,0 +1,56 @@
+<?php declare(strict_types = 1);
+
+namespace Spameri\ElasticQuery\Mapping\Analyzer\Custom\Synonym;
+
+class CzechSynonym extends \Spameri\ElasticQuery\Mapping\Analyzer\Custom\Synonym\AbstractSynonym
+{
+
+	public function name(): string
+	{
+		return 'czechSynonym';
+	}
+
+
+	public function filter(): \Spameri\ElasticQuery\Mapping\Settings\Analysis\FilterCollection
+	{
+		if ( ! $this->filter instanceof \Spameri\ElasticQuery\Mapping\Settings\Analysis\FilterCollection) {
+			$this->filter = new \Spameri\ElasticQuery\Mapping\Settings\Analysis\FilterCollection();
+			$this->filter->add(
+				new \Spameri\ElasticQuery\Mapping\Filter\Lowercase()
+			);
+			if ($this->stopFilter) {
+				$this->filter->add($this->stopFilter);
+
+			} else {
+				$this->filter->add(
+					new \Spameri\ElasticQuery\Mapping\Filter\Stop\Czech()
+				);
+			}
+			$this->filter->add(
+				new \Spameri\ElasticQuery\Mapping\Filter\Synonym(
+					$this->synonyms
+				)
+			);
+			$this->filter->add(
+				new \Spameri\ElasticQuery\Mapping\Filter\Lowercase()
+			);
+			if ($this->stopFilter) {
+				$this->filter->add($this->stopFilter);
+
+			} else {
+				$this->filter->add(
+					new \Spameri\ElasticQuery\Mapping\Filter\Stop\Czech()
+				);
+			}
+			$this->filter->add(
+				new \Spameri\ElasticQuery\Mapping\Filter\Unique()
+			);
+			$this->filter->add(
+				new \Spameri\ElasticQuery\Mapping\Filter\ASCIIFolding()
+			);
+		}
+
+		return $this->filter;
+	}
+
+}
