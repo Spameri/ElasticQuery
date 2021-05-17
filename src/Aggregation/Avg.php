90b0fee3a14fff0fa40afc0b5ee742b14af30368
VBoss <vcevela@gmail.com>
2021-05-17 10:43:21 +0200

    Added AVG aggregation

---
+++ src/Aggregation/Avg.php
@@ -0,0 +1,37 @@
+<?php declare(strict_types = 1);
+
+namespace Spameri\ElasticQuery\Aggregation;
+
+/**
+ * @see https://www.elastic.co/guide/en/elasticsearch/reference/current/search-aggregations-metrics-avg-aggregation.html
+ */
+class Avg implements \Spameri\ElasticQuery\Aggregation\LeafAggregationInterface
+{
+
+	private string $field;
+
+
+	public function __construct(
+		string $field
+	)
+	{
+		$this->field = $field;
+	}
+
+
+	public function key(): string
+	{
+		return 'avg_' . $this->field;
+	}
+
+
+	public function toArray(): array
+	{
+		return [
+			'avg' => [
+				'field' => $this->field,
+			],
+		];
+	}
+
+}
