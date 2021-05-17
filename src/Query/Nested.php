e9e0a5c2b502492e68f200247e868c4033fad0b5
VBoss <vcevela@gmail.com>
2021-05-17 10:41:33 +0200

    Added nested query

---
+++ src/Query/Nested.php
@@ -0,0 +1,51 @@
+<?php declare(strict_types = 1);
+
+namespace Spameri\ElasticQuery\Query;
+
+class Nested implements \Spameri\ElasticQuery\Query\LeafQueryInterface
+{
+
+	private string $path;
+
+	private \Spameri\ElasticQuery\Query\QueryCollection $query;
+
+
+	public function __construct(
+		string $path,
+		?\Spameri\ElasticQuery\Query\QueryCollection $query = NULL
+	) {
+		$this->path = $path;
+
+		if ($query === NULL) {
+			$query = new \Spameri\ElasticQuery\Query\QueryCollection();
+		}
+
+		$this->query = $query;
+	}
+
+
+	public function key(): string
+	{
+		return 'nested_' . $this->path;
+	}
+
+
+	public function toArray(): array
+	{
+		return [
+			'nested' => [
+				'path' => $this->path,
+				'query' => [
+					'bool' => $this->query->toArray(),
+				],
+			],
+		];
+	}
+
+
+	public function getQuery(): \Spameri\ElasticQuery\Query\QueryCollection
+	{
+		return $this->query;
+	}
+
+}
