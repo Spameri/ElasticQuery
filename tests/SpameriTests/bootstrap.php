d4c3fc4acea25e2c25d22775177132ac0089d463
Spamer <vcevela@gmail.com>
2025-12-18 15:18:35 +0100

    bump version and fix url

--- tests/SpameriTests/bootstrap.php
+++ tests/SpameriTests/bootstrap.php
@@ -20,6 +20,6 @@ if ( ! $loader) {
 \date_default_timezone_set('Europe/Prague');
 
 // Elasticsearch host configuration (can be overridden via environment variable)
-\define('ELASTICSEARCH_HOST', \getenv('ELASTICSEARCH_HOST') ?: 'localhost:9200');
+\define('ELASTICSEARCH_HOST', \getenv('ELASTICSEARCH_HOST') ?: 'http://localhost:9200');
 
 return $loader;
